f2aaebaef5a9eb486a3ffd2b444c5e79b58ec02b
Ram.Chander <cram@netapp.com>
2019-01-05 16:16:37 +0530

    change component type

--- src/components/grid/gridRecord.js
+++ src/components/grid/gridRecord.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import * as actionType from '../../actions'
 import DatePicker from "react-datepicker"
@@ -7,80 +7,80 @@ import { FaRegSave, FaEdit, FaTrashAlt } from "react-icons/fa"
 //import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
 import { Input} from 'reactstrap'
 
-const GridRecord = (props) => {
-  
-  var isEdit;
+//const GridRecord = (props) => {
+  class GridRecord extends Component {
 
-  var data = {
-    id:props.id,
-    name_val: props.name_val,
-    order_date: props.order_date,
-    unit: props.unit,
-    discount: props.discount,
-    in_stock: props.in_stock,
+  data = {
+    id:this.props.id,
+    name_val: this.props.name_val,
+    order_date: this.props.order_date,
+    unit: this.props.unit,
+    discount: this.props.discount,
+    in_stock: this.props.in_stock,
   }
 
-  const getData = (evt) => {
+  getData = (evt) => {
     console.log(evt)
     switch(evt.target.name){
       case 'name_a':
-        data.name_val=evt.target.value;
+        this.data.name_val=evt.target.value;
       break;
       case 'text1':
-        data.unit=evt.target.value
+        this.data.unit=evt.target.value
       break;
       case 'text2':
-        data.in_stock=evt.target.value
+        this.data.in_stock=evt.target.value
       break;
     }
     
   }
 
-  const dataSelection = (data) => {  
-    data.order_date=data;
-    document.getElementById("date"+props.id).value=data
+  dataSelection = (data) => {  
+    this.data.order_date=data;
+    document.getElementById("date"+this.props.id).value=data
   }
 
-  const saveData=()=>{    
-    props.addInfo(data);
+  saveData=()=>{    
+    this.props.addInfo(this.data);
   }
 
-  const editData=()=>{
-    document.getElementById("row"+props.id).className = 'flex--cont--def grid--row--container'    
-    document.getElementById("edit"+props.id).className ="displayNone";
-    document.getElementById("save"+props.id).className = "displayBlock";
-    document.getElementById("delete"+props.id).className = "displayBlock";
-    document.getElementById("actionBtn"+props.id).className = "displayNone";
-    //props.editInfo(data);
+  editData=()=>{
+    document.getElementById("row"+this.props.id).className = 'flex--cont--def grid--row--container'    
+    document.getElementById("edit"+this.props.id).className ="displayNone";
+    document.getElementById("save"+this.props.id).className = "displayBlock";
+    document.getElementById("delete"+this.props.id).className = "displayBlock";
+    document.getElementById("actionBtn"+this.props.id).className = "displayNone";
+    //this.props.editInfo(this.data);
   }
 
-  const saveEditedData=()=>{
-    props.editInfo(data);
-    document.getElementById("actionBtn"+props.id).className = "dropdown actionbutton";
-    document.getElementById("edit"+props.id).className ="dropdown-item";
-    document.getElementById("save"+props.id).className = "displayNone";
-    document.getElementById("delete"+props.id).className = "displayNone";
-    document.getElementById("row"+props.id).className = 'flex--cont--def grid--row--container noneEditableGrid'    
+  saveEditedData=()=>{
+    this.props.editInfo(this.props);
+    document.getElementById("actionBtn"+this.props.id).className = "dropdown actionbutton";
+    document.getElementById("edit"+this.props.id).className ="dropdown-item";
+    document.getElementById("save"+this.props.id).className = "displayNone";
+    document.getElementById("delete"+this.props.id).className = "displayNone";
+    document.getElementById("row"+this.props.id).className = 'flex--cont--def grid--row--container noneEditableGrid'    
     
   }
-  const deleteData=()=>{    
-    props.deleteInfo(data);
+  deleteData=()=>{    
+    this.props.deleteInfo(this.props);
   }
 
-  const onDateSelect = () => {
+  onDateSelect = () => {
 
   }
-  const onMouseOverHandler = (evt) => {
+  onMouseOverHandler = (evt) => {
 
   }
 
-  const onMouseOutHandler = () => {
+  onMouseOutHandler = () => {
 
   }
+  render(){
   return (
-      <div id={"row"+props.id} className={props.editMode ? "flex--cont--def grid--row--container":"flex--cont--def grid--row--container noneEditableGrid" }>
+      <div id={"row"+this.props.id} className={this.props.editMode ? "flex--cont--def grid--row--container":"flex--cont--def grid--row--container noneEditableGrid" }>
         <div  className="grid--row--child--container">
-            <input id={"chk_"+props.id} className="chkdiv" type="checkbox" />
+            <input id={"chk_"+this.props.id} className="chkdiv" type="checkbox" />
         </div>    
         <div className="grid--row--child--container">
           {/*<Input type="select" onChange={(evt) => getData(evt)} name="name_a">
@@ -90,20 +90,20 @@ const GridRecord = (props) => {
             <option>4</option>
             <option>5</option>
           </Input>*/}
-          <Input type="text" defaultValue={props.name_val} placeholder="Name" />
+          <Input type="text" defaultValue={this.props.name_val} placeholder="Name" />
         </div>
         <div className="grid--row--child--container">
           <DatePicker 
-            id={'date'+props.id}
+            id={'date'+this.props.id}
             name="txtDate"
             className="grid--calender--field form-control" 
-            selected={new Date(props.order_date)}
-            onChange={(e)=>dataSelection(e)}
+            selected={new Date(this.props.order_date)}
+            onChange={(e)=>this.dataSelection(e)}
           />
-          {/*<Input type="date" name="dateTxt" defaultValue={props.order_date} />*/}
+          {/*<Input type="date" name="dateTxt" defaultValue={this.props.order_date} />*/}
         </div>
         <div className="grid--row--child--container">
-          <Input type="text" onChange={(evt) => getData(evt)} name="text1" defaultValue={props.unit} placeholder="with a placeholder" />
+          <Input type="text" onChange={(evt) => this.getData(evt)} name="text1" defaultValue={this.props.unit} placeholder="with a placeholder" />
         </div>
         <div className="grid--row--child--container">
           <label class="container">
@@ -115,37 +115,37 @@ const GridRecord = (props) => {
           <Input 
             type="text" 
             name="text2"
-           // value={props.in_stock}
-           defaultValue={props.in_stock}
-            onChange={(evt) => getData(evt)}
+           // value={this.props.in_stock}
+           defaultValue={this.props.in_stock}
+            onChange={(evt) => this.getData(evt)}
             placeholder="with a placeholder" />
         </div>
         <div className="grid--row--child--container action--container">
           <div>
           
               
-            {props.newRecord ? 
-              <button onClick={()=> saveData()}>Save</button> 
+            {this.props.newRecord ? 
+              <button onClick={()=> this.saveData()}>Save</button> 
               : 
               (<span>
-                <div class="dropdown actionbutton" id={"actionBtn"+props.id}>
+                <div class="dropdown actionbutton" id={"actionBtn"+this.props.id}>
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <GoKebabVertical size={22} />
                   </button>
                   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <button class="dropdown-item" id={"edit"+props.id} onClick={()=> editData()}><FaEdit size={22} /></button>
-                    <button class="dropdown-item" onClick={()=> deleteData()}><FaTrashAlt size={22} /></button>
+                    <button class="dropdown-item" id={"edit"+this.props.id} onClick={()=> this.editData()}><FaEdit size={22} /></button>
+                    <button class="dropdown-item" onClick={()=> this.deleteData()}><FaTrashAlt size={22} /></button>
                   </div>
                 </div>
-                <button className="displayNone" id={"save"+props.id} onClick={()=> saveEditedData()}><FaRegSave size={22} /></button>
-                <button className="displayNone" id={"delete"+props.id} onClick={()=> deleteData()}><FaTrashAlt size={22} /></button>
+                <button className="displayNone" id={"save"+this.props.id} onClick={()=> this.saveEditData()}><FaRegSave size={22} /></button>
+                <button className="displayNone" id={"delete"+this.props.id} onClick={()=> this.deleteData()}><FaTrashAlt size={22} /></button>
                 </span>)}
             
           </div>
         </div>
 
       </div>
-    )
+    )}
 }
 
 const mapDispatchToProps = dispatch => {
